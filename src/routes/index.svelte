96ff6b0b53e6d5fece69403f8d0a52d1a1797b8e
Adam <chillcoders@gmail.com>
2022-01-19 18:56:22 -0500

    rotate1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,23 @@
 <script>
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+
+	let width = 1;
+	let height = 1;
+	let depth = 1;
+	let spin = 0;
+
+	SC.onFrame(() => {
+		spin = 0.01;
+	});
 </script>
 //cute and red globe! just need text!
 <SC.Canvas antialias background={new THREE.Color('papayawhip')}>
 	<SC.Mesh
 		geometry={new THREE.BoxGeometry()}
 		material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+		scale={[width, height, depth]}
+		rotation={[0, spin, 0]}
 	/>
 <SC.AmbientLight intensity={0.6} />
 	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />

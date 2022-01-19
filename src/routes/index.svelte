517ec1d4d5c76e2cfb08db50a34cfd06389edef2
Adam <chillcoders@gmail.com>
2022-01-19 18:46:50 -0500

    redcube
    solid-shape

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,14 @@
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
 </script>
-
+//cute and red globe! just need text!
 <SC.Canvas antialias background={new THREE.Color('papayawhip')}>
-	<SC.Mesh geometry={new THREE.BoxGeometry()} />
+	<SC.Mesh
+		geometry={new THREE.BoxGeometry()}
+		material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+	/>
+<SC.AmbientLight intensity={0.6} />
+	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
 	<SC.PerspectiveCamera position={[2, 1, 1]} />
+	<SC.OrbitControls enableZoom={false} />
 </SC.Canvas>
